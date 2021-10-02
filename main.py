9305596fdbb07180aecd5a66eb4de231feb852ab
anton <antonw_88@hotmail.com>
2021-10-02 19:13:18 +0200

    upload 2fa exam

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# This is a Python script for a 2fa honeytoken intended for use in a misinformation defense.
 import os
 import time
 import threading
